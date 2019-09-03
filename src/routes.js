9f50823e989d093e04114a31b8aaf873f1831a8f
Victor Batalha <madivcb@gmail.com>
2019-09-02 23:51:04 -0300

    feat: user signup

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/', async (req, res) => {
-  const data = {
-    first_name: 'Victor',
-    last_name: 'Batalha',
-    email: 'madivcb@gmail.com',
-    phone: '51998588292',
-    password: '123456',
-  };
-
-  const user = await User.create(data);
-
-  return res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
