e24a2d3e9d4e4e8255f306a07ba44ed5a867fbab
Victor Batalha <madivcb@gmail.com>
2019-09-03 20:21:10 -0300

    feat: route protection setup

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -8,5 +10,10 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
+
+// Protected routes
+routes.get('/', authMiddleware, (req, res) => {
+  return res.status(200).json({ ok: true });
+});
 
 export default routes;
