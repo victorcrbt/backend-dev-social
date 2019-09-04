f8962714e75991800f1ad31d6808e16f651334d1
Victor Batalha <madivcb@gmail.com>
2019-09-03 21:59:06 -0300

    feat: multer setup

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
 // Protected routes
-routes.get('/', authMiddleware, (req, res) => {
-  return res.status(200).json({ ok: true });
+routes.put('/users', authMiddleware, UserController.update);
+routes.delete('/users', authMiddleware, UserController.delete);
+
+routes.post('/images', upload.single('file'), (req, res) => {
+  return res.status(200).json();
 });
 
 export default routes;
