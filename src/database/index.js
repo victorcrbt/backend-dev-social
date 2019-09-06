64945873d4db55f02956044d182a5a1f8591452e
Victor Batalha <madivcb@gmail.com>
2019-09-05 23:04:01 -0300

    start friends feature

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import databaseConfig from '../config/database';
 
@@ -10,6 +11,7 @@ const models = [User, Avatar];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -18,6 +20,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
   }
 }
 
