214ed64671d3cefafc0a68d570e827d9a94d2455
Victor Batalha <madivcb@gmail.com>
2019-09-17 15:48:47 -0300

    feat: messages model

--- src/database/index.js
+++ src/database/index.js
@@ -7,8 +7,9 @@ import User from '../app/models/User';
 import Avatar from '../app/models/Avatar';
 import Post from '../app/models/Post';
 import Comment from '../app/models/Comment';
+import Message from '../app/models/Message';
 
-const models = [User, Avatar, Post, Comment];
+const models = [User, Avatar, Post, Comment, Message];
 
 class Database {
   constructor() {
